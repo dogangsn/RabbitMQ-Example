d46b37e56d093c64db93981c686f404be48ebe59
Dogan Gunes <dogangns.98@gmail.com>
2023-01-20 18:45:09 +0300

    .

---
+++ Consumer/RabbitMQ-Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("example", exclusive: false, autoDelete: false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += Received;
+channel.BasicConsume(queue: "example", consumer:consumer);
+Console.ReadLine();
+
+void Received(object? sender, BasicDeliverEventArgs e)
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    System.Console.WriteLine($"Recives:{message}");
+}
